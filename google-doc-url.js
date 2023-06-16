5bc6e7335baf62e911dd727ca17e4004712be3cc
Slim1950 <bkbazzell1950@gmail.com>
2023-06-16 05:33:42 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Ez0f1FNZQKA55pzXVaUa2rGlV_XYobUe2dNSm4j_YhA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBHENqnamd0r7socUA0p9vcNBv75JfVYFk';
